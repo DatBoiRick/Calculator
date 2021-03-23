18e1d5f33a39de13c1e4287195b7530ad3477101
DatBoiRick <78659455+DatBoiRick@users.noreply.github.com>
2021-03-22 23:16:09 -0400

    All tests conduct correct operation but read off the work csv

--- src/CSVTests.py
+++ src/CSVTests.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.csv_reader = CsvReader('add.csv')
+        self.csv_reader = CsvReader('subtract.csv')
 
     def test_return_data_as_objects(self):
         people = self.csv_reader.return_data_as_class('person')
